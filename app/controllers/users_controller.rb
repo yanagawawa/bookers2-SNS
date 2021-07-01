5fcd509813e65a8d0c2935352ffa87250ec71807
yanagawawa <yukkebbnba@gmail.com>
2021-07-01 08:47:47 +0000

    userの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,10 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user.id), notice: "You have updated user successfully."
     else
-      render "show"
+      @user = User.find(params[:id])
+      render "edit"
     end
   end
 
