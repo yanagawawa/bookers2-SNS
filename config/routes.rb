5b7cc4e3f54bf1f7dca52564d680ea27c872c6cb
yanagawawa <yukkebbnba@gmail.com>
2021-07-02 05:55:06 +0000

    いいね、コメントのモデルとコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   get 'home/about' => 'homes#about'
-  resources :books
-  resources :users,only: [:show,:index,:edit,:update] do
-   resources :post_comments, only: [:create, :destroy]
+  resources :users,only: [:show,:index,:edit,:update]
+   resources :books do
+   resource :favorites, only: [:create, :destroy]
+   resources :book_comments, only: [:create, :destroy]
   end
   
 end
