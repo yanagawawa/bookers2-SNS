46fcd286e0c22dc0bb01873514808aac02965154
yanagawawa <yukkebbnba@gmail.com>
2021-07-02 05:37:00 +0000

    コメント作成開始

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   get 'home/about' => 'homes#about'
-  resources :users,only: [:show,:index,:edit,:update]
   resources :books
+  resources :users,only: [:show,:index,:edit,:update] do
+   resources :post_comments, only: [:create, :destroy]
+  end
+  
 end
