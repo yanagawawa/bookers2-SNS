006c81e2287f499320b21686bb4cb76e89feaeed
yanagawawa <yukkebbnba@gmail.com>
2021-07-07 05:52:53 +0000

    いいね並び替え実装

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,4 +1,5 @@
 class BookCommentsController < ApplicationController
+  before_action :authenticate_user!
   def create
      @book = Book.find(params[:book_id])
      @comment = current_user.book_comments.new(book_comment_params)
