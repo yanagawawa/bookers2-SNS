a65c11ee758f2dfda2f71a7e3943fea13c2d29ac
yanagawawa <yukkebbnba@gmail.com>
2021-07-10 13:03:14 +0000

    booksテーブルにカテゴリーモデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_09_034937) do
+ActiveRecord::Schema.define(version: 2021_07_10_130117) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 2021_07_09_034937) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "evaluation"
+    t.integer "category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "chats", force: :cascade do |t|
